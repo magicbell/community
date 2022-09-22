776dad04628c2b8032e5012d2f81b4a9c38fec2a
Stephan Meijer <stephan.meijer@gmail.com>
2022-09-22 21:46:47 +0200

    feat: verify that operationIds match convention

--- scripts/validate.ts
+++ scripts/validate.ts
@@ -3,6 +3,7 @@ import 'zx/globals';
 
 import swagger from '@apidevtools/swagger-parser';
 import jsonpath from 'jsonpath';
+import { OpenAPIV3 } from 'openapi-types';
 
 const showHelp = argv.help;
 const specFile = argv.spec || 'spec/openapi.json';
@@ -14,14 +15,50 @@ if (showHelp) {
 
 const spec = await fs.readJSON(path.resolve(specFile));
 
-const api = await swagger.validate(spec as any).catch((error) => {
+const api = await swagger.dereference(spec as any).catch((error) => {
   console.error(`Error: ${specFile} did not pass swagger validation!`, error.message);
   process.exit(1);
 });
 
-const operationIds = jsonpath.query(api, '$..operationId');
-const duplicateOperationIds = operationIds.filter((item, index) => operationIds.indexOf(item) !== index);
-if (duplicateOperationIds.length > 0) {
-  console.error(`Error: ${specFile} contains duplicate operationIds!`, duplicateOperationIds);
-  process.exit(1);
+const errors: string[] = [];
+
+function assertUniqueOperationIds() {
+  const operationIds = jsonpath.query(api, '$..operationId');
+  const duplicateOperationIds = operationIds.filter((item, index) => operationIds.indexOf(item) !== index);
+
+  for (const operationId of duplicateOperationIds) {
+    errors.push(`Operation ID "${operationId}" is not unique.`);
+  }
 }
+
+function assertOperationIdsMatchConvention() {
+  for (const [path, pathObj] of Object.entries(api.paths)) {
+    for (const [method, operation] of Object.entries<OpenAPIV3.OperationObject>(pathObj)) {
+      const operationId = operation.operationId;
+      if (!operationId) {
+        errors.push(`Operation ID is missing for ${path}/${method}.`);
+      }
+
+      const entity = path.split('/').filter(Boolean)[0].replace(/_/g, '-');
+      const singleEntity = entity.replace(/s$/, '');
+      const isNamespaced = operationId.startsWith(entity + '-');
+      const repeatsEntity = operationId
+        .substr(entity.length + 1)
+        .split('-')
+        .some((segment, idx) => idx > 0 && (segment === singleEntity || segment === entity));
+
+      if (!isNamespaced || repeatsEntity) {
+        errors.push(`Operation ID "${operationId}" does not follow the convention of {namespace}-{method-name}.`);
+      }
+    }
+  }
+}
+
+assertUniqueOperationIds();
+assertOperationIdsMatchConvention();
+
+for (const error of errors) {
+  console.log(chalk.redBright(error));
+}
+
+process.exit(errors.length ? 1 : 0);
