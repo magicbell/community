9099c00f9a7cfd9cafb6b70fdc8c2aa3cd66d271
Stephan Meijer <stephan.meijer@gmail.com>
2022-09-22 15:07:02 +0200

    feat: add duplicate operationId detection

--- scripts/validate.ts
+++ scripts/validate.ts
@@ -2,6 +2,7 @@
 import 'zx/globals';
 
 import swagger from '@apidevtools/swagger-parser';
+import jsonpath from 'jsonpath';
 
 const showHelp = argv.help;
 const specFile = argv.spec || 'spec/openapi.json';
@@ -11,10 +12,16 @@ if (showHelp) {
   process.exit(0);
 }
 
-try {
-  const spec = await fs.readJSON(path.resolve(specFile));
-  const { info } = await swagger.validate(spec as any);
-  console.log(`API name: ${info.title}, version: ${info.version}`);
-} catch (error) {
+const spec = await fs.readJSON(path.resolve(specFile));
+
+const api = await swagger.validate(spec as any).catch((error) => {
   console.error(`Error: ${specFile} did not pass swagger validation!`, error.message);
+  process.exit(1);
+});
+
+const operationIds = jsonpath.query(api, '$..operationId');
+const duplicateOperationIds = operationIds.filter((item, index) => operationIds.indexOf(item) !== index);
+if (duplicateOperationIds.length > 0) {
+  console.error(`Error: ${specFile} contains duplicate operationIds!`, duplicateOperationIds);
+  process.exit(1);
 }
