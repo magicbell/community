462e4269f2b167f99cb6001b7dac67a151758392
Hana Mohan <unamashana@gmail.com>
2022-12-12 22:05:32 +0100

    fixes the import

--- docs/scripts/refresh-openapi.ts
+++ docs/scripts/refresh-openapi.ts
@@ -2,14 +2,14 @@
 import { mkdir } from 'fs/promises';
 import { dirname } from 'path';
 import 'zx/globals';
-import { API_SPEC_FILE, fetchOpenAPISpec } from '../lib/openapi';
+import { fetchOpenAPISpec, OPENAPI_FILE } from '../lib/openapi';
 
 async function main() {
   const spec = await fetchOpenAPISpec({ force: true });
 
-  await mkdir(dirname(API_SPEC_FILE), { recursive: true });
-  await fs.writeJSON(API_SPEC_FILE, spec, { spaces: 2 });
-  console.log(`openapi: refreshed ${API_SPEC_FILE}`);
+  await mkdir(dirname(OPENAPI_FILE), { recursive: true });
+  await fs.writeJSON(OPENAPI_FILE, spec, { spaces: 2 });
+  console.log(`openapi: refreshed ${OPENAPI_FILE}`);
 }
 
 main();
