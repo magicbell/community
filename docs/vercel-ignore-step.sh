d860ac8355cb0ff225f0b7e348a56d8bcfcb3ef8
3v0k4 <riccardo.odone@gmail.com>
2022-12-13 12:58:32 +0100

    wip

--- docs/vercel-ignore-step.sh
+++ docs/vercel-ignore-step.sh
@@ -3,20 +3,20 @@
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]]; then
-  echo "✅ - Build can proceed"
+  echo "✅ Build can proceed"
   exit 1;
 
 else
-  git diff --quiet HEAD main -- docs/ openapi/
+  git diff --quiet HEAD^ HEAD -- ../docs/ ../openapi/
   CHANGED=$?
-  echo "git diff --quiet main HEAD -- docs/ openapi/: $CHANGED"
+  echo "git diff --quiet HEAD^ HEAD -- ../docs/ ../openapi/: $CHANGED"
 
   if [[ $CHANGED == 0 ]]; then
-    echo "🛑 - Build cancelled"
+    echo "🛑 Build cancelled"
     exit 0;
 
   else
-    echo "✅ - Build can proceed"
+    echo "✅ Build can proceed"
     exit 1;
   fi
 fi
