634557e7dc5fb8b47eceff08eaa01cdef98e391e
Stephan Meijer <stephan.meijer@gmail.com>
2023-10-12 11:51:43 +0200

    chore: remove code that has been moved to private repo

--- docs/src/components/Table.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react';
-
-export default function Table({ children }: { children: ReactNode }) {
-  return (
-    <div className="overflow-hidden rounded-md border border-borderMuted my-4">
-      <table>{children}</table>
-    </div>
-  );
-}
